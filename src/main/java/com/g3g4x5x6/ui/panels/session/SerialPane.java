5b435be9d42e369b67ba17d39628109d5fa2cc54
勾三股四弦五小六 <87740076+G3G4X5X6@users.noreply.github.com>
2021-09-14 17:48:12 +0800

    update SerialPane

--- src/main/java/com/g3g4x5x6/ui/panels/session/SerialPane.java
+++ src/main/java/com/g3g4x5x6/ui/panels/session/SerialPane.java
@@ -12,8 +12,8 @@ import java.awt.event.ActionEvent;
 
 @Slf4j
 public class SerialPane extends JPanel {
+    private static final int[] FLOW_CONTROL = new int[]{0, 1, 16, 256, 4096, 65536, 1048576};
     private JTabbedPane mainTabbedPane;
-
     private JTabbedPane basicSettingTabbedPane;
     private String basicSettingPaneTitle;
     private JPanel basicSettingPane;
@@ -26,7 +26,8 @@ public class SerialPane extends JPanel {
     private JComboBox<String> rateComboBox;
     private JComboBox<String> dataBitComboBox;
     private JComboBox<String> stopBitComboBox;
-    private JComboBox<String> ccBitComboBox;
+    private JComboBox<String> parityBitComboBox;
+    private JComboBox<String> fcComboBox;
 
     public SerialPane(JTabbedPane mainTabbedPane){
         this.mainTabbedPane = mainTabbedPane;
@@ -61,7 +62,8 @@ public class SerialPane extends JPanel {
         if (SerialPort.getCommPorts().length >= 1){
             comComboBox.removeAllItems();
             for (SerialPort comPort : SerialPort.getCommPorts()){
-                comComboBox.addItem(comPort.getSystemPortName());
+                log.debug(comPort.getDescriptivePortName());
+                comComboBox.addItem(comPort.getDescriptivePortName());
             }
         }
         comComboBox.setEditable(true);
@@ -91,33 +93,46 @@ public class SerialPane extends JPanel {
         // 停止位
         JPanel stopBitPane = new JPanel();
         JLabel stopBitLabel = new JLabel("停止位*");
-        String[] defaultStopBit = new String[]{"1", "1.5", "2"};
+        String[] defaultStopBit = new String[]{"1", "2"};
         stopBitComboBox = new JComboBox<>(defaultStopBit);
         stopBitComboBox.setEditable(true);
         stopBitPane.add(stopBitLabel);
         stopBitPane.add(stopBitComboBox);
 
         // 校验位
-        JPanel ccBitPane = new JPanel();
-        JLabel ccBitLabel = new JLabel("校验位*");
-        String[] defaultCcBit = new String[]{"None", "0", "1"};
-        ccBitComboBox = new JComboBox<>(defaultCcBit);
-        ccBitComboBox.setEditable(true);
-        ccBitPane.add(ccBitLabel);
-        ccBitPane.add(ccBitComboBox);
+//        public static final int NO_PARITY = 0;
+//        public static final int ODD_PARITY = 1;
+//        public static final int EVEN_PARITY = 2;
+//        public static final int MARK_PARITY = 3;
+//        public static final int SPACE_PARITY = 4;
+        JPanel parityBitPane = new JPanel();
+        JLabel parityBitLabel = new JLabel("奇偶校验*");
+        String[] defaultParityBit = new String[]{"NO", "ODD", "EVEN", "MARK", "SPACE"};
+        parityBitComboBox = new JComboBox<>(defaultParityBit);
+        parityBitComboBox.setEditable(true);
+        parityBitPane.add(parityBitLabel);
+        parityBitPane.add(parityBitComboBox);
 
         // 按钮
         JPanel btnPane = new JPanel();
         JButton saveBtn = new JButton("快速连接");
+        saveBtn.setToolTipText("自动保存会话");
         saveBtn.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 log.debug("Serial 快速连接");
-                mainTabbedPane.insertTab("Serial-" + comComboBox.getSelectedItem(), new FlatSVGIcon("com/g3g4x5x6/ui/icons/OpenTerminal_13x13.svg"),
-                        new JPanel(),
-                        "Serial-" + comComboBox.getSelectedItem(),
-                        mainTabbedPane.getSelectedIndex());
-                mainTabbedPane.removeTabAt(mainTabbedPane.getSelectedIndex());
+                if (testOpen()){
+                    // TODO 保存会话
+
+                    // 打开会话
+                    mainTabbedPane.insertTab("Serial-" + comComboBox.getSelectedItem(), new FlatSVGIcon("com/g3g4x5x6/ui/icons/OpenTerminal_13x13.svg"),
+                            new JPanel(),
+                            "Serial-" + comComboBox.getSelectedItem(),
+                            mainTabbedPane.getSelectedIndex());
+                    mainTabbedPane.removeTabAt(mainTabbedPane.getSelectedIndex());
+                }else{
+                    DialogUtil.warn("Serial is not Open!");
+                }
             }
         });
         JButton testBtn = new JButton("测试通信");
@@ -144,20 +159,46 @@ public class SerialPane extends JPanel {
         basicSettingPane.add(Box.createHorizontalGlue());
         basicSettingPane.add(stopBitPane);
         basicSettingPane.add(Box.createHorizontalGlue());
-        basicSettingPane.add(ccBitPane);
+        basicSettingPane.add(parityBitPane);
         basicSettingPane.add(Box.createHorizontalGlue());
         basicSettingPane.add(btnPane);
     }
 
     private void initAdvancePane(){
         advancedSettingTabbedPane.addTab(advancedSettingPaneTitle, advancedSettingPane);
+
+        // 流控
+//        public static final int FLOW_CONTROL_DISABLED = 0;
+//        public static final int FLOW_CONTROL_RTS_ENABLED = 1;
+//        public static final int FLOW_CONTROL_CTS_ENABLED = 16;
+//        public static final int FLOW_CONTROL_DSR_ENABLED = 256;
+//        public static final int FLOW_CONTROL_DTR_ENABLED = 4096;
+//        public static final int FLOW_CONTROL_XONXOFF_IN_ENABLED = 65536;
+//        public static final int FLOW_CONTROL_XONXOFF_OUT_ENABLED = 1048576;
+        JPanel fcPane = new JPanel();
+        JLabel fcLabel = new JLabel("流控*");
+        String[] defaultFc = new String[]{"DISABLED", "RTS", "CTS", "DSR", "DTR", "XONXOFF_IN", "XONXOFF_OUT"};
+        fcComboBox = new JComboBox<>(defaultFc);
+        fcComboBox.setEditable(true);
+        fcPane.add(fcLabel);
+        fcPane.add(fcComboBox);
+
+        advancedSettingPane.add(fcPane);
     }
 
     private Boolean testOpen(){
         Boolean flag = false;
         for (SerialPort comPort : SerialPort.getCommPorts()){
-            if (comPort.getSystemPortName().equals(comComboBox.getSelectedItem().toString()))
-                flag = comPort.openPort();
+            if (comPort.getSystemPortName().equals(comComboBox.getSelectedItem().toString())){
+                comPort.setBaudRate(Integer.valueOf(rateComboBox.getSelectedItem().toString()));
+                comPort.setNumDataBits(Integer.valueOf(dataBitComboBox.getSelectedItem().toString()));
+                comPort.setNumStopBits(Integer.valueOf(stopBitComboBox.getSelectedItem().toString()));
+                comPort.setParity(parityBitComboBox.getSelectedIndex());
+                comPort.setFlowControl(FLOW_CONTROL[fcComboBox.getSelectedIndex()]);
+                comPort.openPort();
+                flag = comPort.isOpen();
+                comPort.closePort();
+            }
         }
         return flag;
     }
