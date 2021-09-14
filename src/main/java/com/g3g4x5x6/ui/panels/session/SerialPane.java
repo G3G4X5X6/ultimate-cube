74df0d083b37f377da28d9171ceee057b64fd2a0
勾三股四弦五小六 <87740076+G3G4X5X6@users.noreply.github.com>
2021-09-14 11:44:11 +0800

    update SerialPane

--- src/main/java/com/g3g4x5x6/ui/panels/session/SerialPane.java
+++ src/main/java/com/g3g4x5x6/ui/panels/session/SerialPane.java
@@ -1,8 +1,13 @@
 package com.g3g4x5x6.ui.panels.session;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
+
+@Slf4j
 public class SerialPane extends JPanel {
     private JTabbedPane basicSettingTabbedPane;
     private String basicSettingPaneTitle;
@@ -14,6 +19,9 @@ public class SerialPane extends JPanel {
 
     private JComboBox<String> comComboBox;
     private JComboBox<String> rateComboBox;
+    private JComboBox<String> dataBitComboBox;
+    private JComboBox<String> stopBitComboBox;
+    private JComboBox<String> ccBitComboBox;
 
     public SerialPane(){
         this.setLayout(new BorderLayout());
@@ -44,6 +52,8 @@ public class SerialPane extends JPanel {
         String[] defaultComs = new String[]{"COM1", "COM2", "COM3", "COM4", "COM5", "COM6"};
         comComboBox = new JComboBox<>(defaultComs);
         comComboBox.setEditable(true);
+        comPane.add(comLabel);
+        comPane.add(comComboBox);
 
         // 波特率
         JPanel ratePane = new JPanel();
@@ -51,25 +61,67 @@ public class SerialPane extends JPanel {
         String[] defaultRates = new String[]{"1200", "2400", "4800", "9600"};
         rateComboBox = new JComboBox<>(defaultRates);
         rateComboBox.setEditable(true);
-
-        // 数据位
-
-        // 停止位
-
-        // 校验位
-
-        // 校验类型
-
-        // Hex/ASCII 发送和显示
-
-        comPane.add(comLabel);
-        comPane.add(comComboBox);
         ratePane.add(rateLabel);
         ratePane.add(rateComboBox);
+
+        // 数据位
+        JPanel dataBitPane = new JPanel();
+        JLabel dataBitLabel = new JLabel("数据位*");
+        String[] defaultDataBit = new String[]{"5", "6", "7", "8"};
+        dataBitComboBox = new JComboBox<>(defaultDataBit);
+        dataBitComboBox.setEditable(true);
+        dataBitPane.add(dataBitLabel);
+        dataBitPane.add(dataBitComboBox);
+
+        // 停止位
+        JPanel stopBitPane = new JPanel();
+        JLabel stopBitLabel = new JLabel("停止位*");
+        String[] defaultStopBit = new String[]{"1", "1.5", "2"};
+        stopBitComboBox = new JComboBox<>(defaultStopBit);
+        stopBitComboBox.setEditable(true);
+        stopBitPane.add(stopBitLabel);
+        stopBitPane.add(stopBitComboBox);
+
+        // 校验位
+        JPanel ccBitPane = new JPanel();
+        JLabel ccBitLabel = new JLabel("校验位*");
+        String[] defaultCcBit = new String[]{"None", "0", "1"};
+        ccBitComboBox = new JComboBox<>(defaultCcBit);
+        ccBitComboBox.setEditable(true);
+        ccBitPane.add(ccBitLabel);
+        ccBitPane.add(ccBitComboBox);
+
+        // 按钮
+        JPanel btnPane = new JPanel();
+        JButton saveBtn = new JButton("快速连接");
+        saveBtn.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                log.debug("Serial 快速连接");
+            }
+        });
+        JButton testBtn = new JButton("测试通信");
+        testBtn.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                log.debug("Serial 测试通信");
+            }
+        });
+        btnPane.add(saveBtn);
+        btnPane.add(testBtn);
+
+
         basicSettingPane.add(comPane);
         basicSettingPane.add(Box.createHorizontalGlue());
         basicSettingPane.add(ratePane);
-
+        basicSettingPane.add(Box.createHorizontalGlue());
+        basicSettingPane.add(dataBitPane);
+        basicSettingPane.add(Box.createHorizontalGlue());
+        basicSettingPane.add(stopBitPane);
+        basicSettingPane.add(Box.createHorizontalGlue());
+        basicSettingPane.add(ccBitPane);
+        basicSettingPane.add(Box.createHorizontalGlue());
+        basicSettingPane.add(btnPane);
     }
 
     private void initAdvancePane(){
