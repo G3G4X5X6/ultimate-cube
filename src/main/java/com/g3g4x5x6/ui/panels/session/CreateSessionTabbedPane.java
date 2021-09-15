39ebcf9ff84d240449e72cbca658a46decb0c86c
g3g4x5x6 <1822485512@qq.com>
2021-09-15 21:26:47 +0800

    added TelnetPane

--- src/main/java/com/g3g4x5x6/ui/panels/session/CreateSessionTabbedPane.java
+++ src/main/java/com/g3g4x5x6/ui/panels/session/CreateSessionTabbedPane.java
@@ -1,6 +1,7 @@
 package com.g3g4x5x6.ui.panels.session;
 
 import com.g3g4x5x6.ui.panels.serial.SerialPane;
+import com.g3g4x5x6.ui.panels.telnet.TelnetPane;
 
 import javax.swing.*;
 
@@ -9,7 +10,7 @@ public class CreateSessionTabbedPane extends JTabbedPane {
     public CreateSessionTabbedPane(JTabbedPane mainTabbedPane) {
         this.addTab("SSH", new SshPane(mainTabbedPane));
         this.addTab("Serial", new SerialPane(mainTabbedPane));
-        this.addTab("Telnet", new JPanel());
+        this.addTab("Telnet", new TelnetPane(mainTabbedPane));
         this.addTab("RDP", new JPanel());
         this.addTab("SFTP", new JPanel());
         this.addTab("FTP", new JPanel());
