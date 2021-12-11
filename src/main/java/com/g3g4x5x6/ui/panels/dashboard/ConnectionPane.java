09b6ff744b64ea160f56f8a78beef7896865ac64
18312091753 <18312091753@163.com>
2021-12-11 14:48:48 +0800

    添加过滤出外网IP的开关按钮

--- src/main/java/com/g3g4x5x6/ui/panels/dashboard/ConnectionPane.java
+++ src/main/java/com/g3g4x5x6/ui/panels/dashboard/ConnectionPane.java
@@ -2,6 +2,7 @@ package com.g3g4x5x6.ui.panels.dashboard;
 
 import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.formdev.flatlaf.extras.components.FlatButton;
+import com.formdev.flatlaf.extras.components.FlatToggleButton;
 import com.g3g4x5x6.utils.CommonUtil;
 import com.g3g4x5x6.utils.ConfigUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -20,8 +22,10 @@ import java.io.IOException;
 @Slf4j
 public class ConnectionPane extends JPanel {
     private BorderLayout borderLayout = new BorderLayout();
+    private FlowLayout flowLayout = new FlowLayout();
 
     private JToolBar toolBar;
+    private FlatToggleButton netBtn;
 
     private JScrollPane scrollPane;
     private JTable table;
@@ -30,9 +34,24 @@ public class ConnectionPane extends JPanel {
 
     public ConnectionPane() {
         this.setLayout(borderLayout);
-        
+
         toolBar = new JToolBar();
         toolBar.setFloatable(false);
+
+        netBtn = new FlatToggleButton();
+        netBtn.setIcon(new FlatSVGIcon("com/g3g4x5x6/ui/icons/activity.svg"));
+        netBtn.setSelected(true);
+        netBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        flushWinTable();
+                    }
+                }).start();
+            }
+        });
 
         FlatButton freshBtn = new FlatButton();
         freshBtn.setButtonType(FlatButton.ButtonType.toolBarButton);
@@ -49,6 +68,8 @@ public class ConnectionPane extends JPanel {
             }
         });
 
+        toolBar.add(netBtn);
+        toolBar.add(Box.createHorizontalGlue());
         toolBar.add(freshBtn);
 
         table = new JTable();
@@ -66,15 +87,7 @@ public class ConnectionPane extends JPanel {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    try {
-                        flushWinTable();
-                        Thread.sleep(1000 * 60*10);
-                        log.debug("刷新网络连接");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+                flushWinTable();
             }
         }).start();
 
@@ -118,6 +131,9 @@ public class ConnectionPane extends JPanel {
                 if (!ip.strip().equals("0.0.0.0") && !ip.strip().equals("*") && !ip.strip().startsWith("[") && !ip.strip().equals("127.0.0.1")){
                     String ipAndInfo = ip + "(" + CommonUtil.queryIp(ip) + ")";
                     row[2] = ipAndInfo;
+                } else if (netBtn.isSelected()){
+                    log.debug("TCP ALL: continue");
+                    continue;
                 }
                 if (ip.strip().equals("127.0.0.1")){
                     String ipAndInfo = ip + "(本机地址,  CZ88.NET)";
@@ -130,9 +146,12 @@ public class ConnectionPane extends JPanel {
                 String[] tmpRow = line.strip().split("\\s+");
                 String[] row = new String[]{tmpRow[0], tmpRow[1], tmpRow[2], "", tmpRow[3]};
                 String ip = row[2].split(":")[0];
-                if (!ip.strip().equals("0.0.0.0") && !ip.strip().equals("*") && !ip.strip().startsWith("[")){
+                if (!ip.strip().equals("0.0.0.0") && !ip.strip().equals("*") && !ip.strip().startsWith("[") && netBtn.isSelected()){
                     String ipAndInfo = ip + "(" + CommonUtil.queryIp(ip) + ")";
                     row[2] = ipAndInfo;
+                    log.debug("UDP: " + ipAndInfo);
+                }else {
+                    continue;
                 }
                 tableModel.addRow(row);
             }
