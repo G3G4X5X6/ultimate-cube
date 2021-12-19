255433f2986eb520ad24b2e1a67ec8959d65b91f
18312091753 <18312091753@163.com>
2021-12-20 00:15:35 +0800

    update

--- src/main/java/com/g3g4x5x6/ui/panels/ssh/SshTabbedPane.java
+++ src/main/java/com/g3g4x5x6/ui/panels/ssh/SshTabbedPane.java
@@ -11,10 +11,9 @@ import javax.swing.*;
 import static com.formdev.flatlaf.FlatClientProperties.TABBED_PANE_TRAILING_COMPONENT;
 
 public class SshTabbedPane extends JTabbedPane {
-    private JTabbedPane mainTabbedPane;
 
     public SshTabbedPane(JTabbedPane mainTabbedPane, JediTermWidget Ssh, String hostField, String portField, String userField, String passField) {
-        this.mainTabbedPane = mainTabbedPane;
+        // TODO
         this.addTab("SSH", Ssh);
         this.addTab("SFTP", new SftpPane(hostField, portField, userField, passField));
         this.addTab("Monitor", new MonitorPane(hostField, Integer.parseInt(portField), userField, passField));
@@ -26,24 +25,23 @@ public class SshTabbedPane extends JTabbedPane {
     private void customComponents() {
         JToolBar leading = null;
         JToolBar trailing = null;
-//        if( leadingComponentButton.isSelected() ) {
-            leading = new JToolBar();
-            leading.setFloatable( false );
-            leading.setBorder( null );
-            leading.add( new JButton( new FlatSVGIcon( "com/g3g4x5x6/ui/icons/project.svg" ) ) );
-//        }
-//        if( trailingComponentButton.isSelected() ) {
-            trailing = new JToolBar();
-            trailing.setFloatable( false );
-            trailing.setBorder( null );
-            trailing.add( new JButton( new FlatSVGIcon( "com/g3g4x5x6/ui/icons/buildLoadChanges.svg" ) ) );
-            trailing.add( Box.createHorizontalGlue() );
-            trailing.add( new JButton( new FlatSVGIcon( "com/g3g4x5x6/ui/icons/commit.svg" ) ) );
-            trailing.add( new JButton( new FlatSVGIcon( "com/g3g4x5x6/ui/icons/diff.svg" ) ) );
-            trailing.add( new JButton( new FlatSVGIcon( "com/g3g4x5x6/ui/icons/listFiles.svg" ) ) );
-//        }
+
+        leading = new JToolBar();
+        leading.setFloatable(false);
+        leading.setBorder(null);
+        leading.add(new JButton(new FlatSVGIcon("com/g3g4x5x6/ui/icons/project.svg")));
+
+        trailing = new JToolBar();
+        trailing.setFloatable(false);
+        trailing.setBorder(null);
+        trailing.add(new JButton(new FlatSVGIcon("com/g3g4x5x6/ui/icons/buildLoadChanges.svg")));
+        trailing.add(Box.createHorizontalGlue());
+        trailing.add(new JButton(new FlatSVGIcon("com/g3g4x5x6/ui/icons/commit.svg")));
+        trailing.add(new JButton(new FlatSVGIcon("com/g3g4x5x6/ui/icons/diff.svg")));
+        trailing.add(new JButton(new FlatSVGIcon("com/g3g4x5x6/ui/icons/listFiles.svg")));
+
 //        this.putClientProperty( TABBED_PANE_LEADING_COMPONENT, leading );
-        this.putClientProperty( TABBED_PANE_TRAILING_COMPONENT, trailing );
+        this.putClientProperty(TABBED_PANE_TRAILING_COMPONENT, trailing);
     }
 
 }
