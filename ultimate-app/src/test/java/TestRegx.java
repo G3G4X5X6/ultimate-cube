6c0cb8fd5c92eda4526c3a2e18798d9abc8f7d3c
G3G4X5X6 <87740076+G3G4X5X6@users.noreply.github.com>
2022-09-11 20:28:07 +0800

    update test

--- ultimate-app/src/test/java/TestRegx.java
+++ ultimate-app/src/test/java/TestRegx.java
@@ -1,25 +1,25 @@
 import org.junit.jupiter.api.Test;
 
 public class TestRegx {
-    @Test
-    public void testRegx() {
-        // The first regex evaluation will never end in JDK <= 9
-        // and the second regex evaluation will never end in any versions of the JDK:
-
-//        java.util.regex.Pattern.compile("(a+)+").matcher(
-//                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
-//                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
-//                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
-//                        "aaaaaaaaaaaaaaa!").matches(); // Sensitive
-
-        java.util.regex.Pattern.compile("(h|h|ih(((i|a|c|c|a|i|i|j|b|a|i|b|a|a|j))+h)ahbfhba|c|i)*").matcher(
-                "hchcchicihcchciiicichhcichcihcchiihichiciiiihhcchi" +
-                        "cchhcihchcihiihciichhccciccichcichiihcchcihhicchcciicchcccihiiihhihihihi" +
-                        "chicihhcciccchihhhcchichchciihiicihciihcccciciccicciiiiiiiiicihhhiiiihchccch" +
-                        "chhhhiiihchihcccchhhiiiiiiiicicichicihcciciihichhhhchihciiihhiccccccciciihh" +
-                        "ichiccchhicchicihihccichicciihcichccihhiciccccccccichhhhihihhcchchihih" +
-                        "iihhihihihicichihiiiihhhhihhhchhichiicihhiiiiihchccccchichci").matches(); // Sensitive
-
-        System.out.println("Over!");
-    }
+//    @Test
+//    public void testRegx() {
+//        // The first regex evaluation will never end in JDK <= 9
+//        // and the second regex evaluation will never end in any versions of the JDK:
+//
+////        java.util.regex.Pattern.compile("(a+)+").matcher(
+////                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+////                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+////                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+////                        "aaaaaaaaaaaaaaa!").matches(); // Sensitive
+//
+//        java.util.regex.Pattern.compile("(h|h|ih(((i|a|c|c|a|i|i|j|b|a|i|b|a|a|j))+h)ahbfhba|c|i)*").matcher(
+//                "hchcchicihcchciiicichhcichcihcchiihichiciiiihhcchi" +
+//                        "cchhcihchcihiihciichhccciccichcichiihcchcihhicchcciicchcccihiiihhihihihi" +
+//                        "chicihhcciccchihhhcchichchciihiicihciihcccciciccicciiiiiiiiicihhhiiiihchccch" +
+//                        "chhhhiiihchihcccchhhiiiiiiiicicichicihcciciihichhhhchihciiihhiccccccciciihh" +
+//                        "ichiccchhicchicihihccichicciihcichccihhiciccccccccichhhhihihhcchchihih" +
+//                        "iihhihihihicichihiiiihhhhihhhchhichiicihhiiiiihchccccchichci").matches(); // Sensitive
+//
+//        System.out.println("Over!");
+//    }
 }
